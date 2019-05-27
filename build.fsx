f9a8dd48dd1cddd5c66b3e202f4376f9728989c0
Shane Charles <shanecharles@burningicesolutions.com>
2019-05-27 06:35:14 -0500

    Change the github project name.

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@
 let gitOwner = "shanecharles"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "intro-property-based-testing-prdc2018"
+let gitProjectName = "level-up-with-generative-testing-2019"
 // The name of the GitHub repo subdirectory to publish slides to
 let gitSubDir = ""
 
